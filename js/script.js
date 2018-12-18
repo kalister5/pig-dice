68fdf1e067963cc823912797d7b28ea634536be3
kalister5 <zainkalister@gmail.com>
2018-12-18 08:45:09 +0300

    added a javascript function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-
-var throwdice = function () {
-  return Math.floor(6*Math.random())+1;
+var minNum = 1;
+var maxNum = 6;
+var randomNum =function(){
+   return Math.floor(Math.random() * (+maxNum - +minNum)) + +minNum;
 }
+var totalPoints =
+function rollDiceb() {
+ var random = randomNum();
+ if (random != 1) {
+   $("#p1points").text(random);
+ } else {
+   $("#p2points").text(random);
+ }
+};
